e76b38e79c883c4ad770bfb11c664bf8a4f36c5b
davidtnfsh <davidtnfsh@gmail.com>
2023-03-16 22:58:56 +0800

    test

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
   required_version = ">= 1.1.0"
 
   cloud {
-    organization = "REPLACE_ME"
+    organization = "example-org-d63b9a"
 
     workspaces {
       name = "gh-actions-demo"
